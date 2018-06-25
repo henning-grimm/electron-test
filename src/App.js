c48dad4db63e91b925c9405d8be398562f719098
Henning Grimm <grimm@gestalt.systems>
2018-06-25 23:01:09 +0200

    bump version

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          This is a new text
+          This is a new text with some more stuff here...
         </p>
       </div>
     );
